a305b14da7700067ec6e93c1481deee11101807c
Allen Geary <ageary@morelandlabs.com>
2016-03-06 18:17:58 -0500

    forgot file

---
+++ libraries/serviceProviderInterface/src/com/morelandLabs/page/StepStatus.java
@@ -0,0 +1,17 @@
+package com.morelandLabs.page;
+
+/**
+ * The Enum StepStatus.
+ */
+public enum StepStatus
+{
+    
+    /** The success. */
+    SUCCESS,
+    
+    /** The failure. */
+    FAILURE,
+    
+    /** The failure ignored. */
+    FAILURE_IGNORED;
+}
