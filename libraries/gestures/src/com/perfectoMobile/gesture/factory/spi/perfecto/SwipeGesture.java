1b659c0dadd4908ee61344945a961c73ee778b03
Allen Geary <ageary@morelandlabs.com>
2016-03-22 09:28:42 -0400

    Latest build information

--- libraries/gestures/src/com/perfectoMobile/gesture/factory/spi/perfecto/SwipeGesture.java
+++ libraries/gestures/src/com/perfectoMobile/gesture/factory/spi/perfecto/SwipeGesture.java
@@ -30,9 +30,6 @@ public class SwipeGesture extends AbstractSwipeGesture
 			{
 				if ( webElement.getLocation() != null && webElement.getSize() != null && webElement.getSize().getWidth() > 0 && webElement.getSize().getHeight() > 0 )
 				{
-					
-					
-					
 					int x = (int)( ( getSwipeStart().getX() / 100.0 ) * (double) webElement.getSize().getWidth() + webElement.getLocation().getX() );
 					int y = (int) ( ( getSwipeStart().getY() / 100.0 ) * (double) webElement.getSize().getHeight() + webElement.getLocation().getY() );
 					Point swipeStart = new Point( x, y );
