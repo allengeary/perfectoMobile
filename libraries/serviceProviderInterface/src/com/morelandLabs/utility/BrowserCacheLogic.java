97fc8116d1603b883e8c3332a0eb7ae79754cb1c
Jim Harrington <jim.r.harrington@g,ail.com>
2016-04-05 22:54:44 -0400

    checkpoint

--- libraries/serviceProviderInterface/src/com/morelandLabs/utility/BrowserCacheLogic.java
+++ libraries/serviceProviderInterface/src/com/morelandLabs/utility/BrowserCacheLogic.java
@@ -15,97 +15,121 @@ public class BrowserCacheLogic
     {
         HashMap<String, Object> params = new HashMap();
 
-        switchToContext(driver, "NATIVE_APP");
+        String currentContext = getCurrentContextHandle( driver );
 
-        //
-        // get os version
-        //
-        
-        params.put("property", "osVersion");
-        String osVerStr = (String) driver.executeScript("mobile:handset:info", params);
-        int[] osVer = parseVersion( osVerStr );
-        
-
-        //
-        // Launch Settings Application on it's main page
-        //
-        
-        params.clear();
-        params.put("name", "Settings");
-        try {
-            driver.executeScript("mobile:application:close", params);
-        } catch (Exception e) {}
-        driver.executeScript("mobile:application:open", params);
-        sleep(1000);
-
-        //
-        // Scroll to Top
-        //
-        
-        params.clear();
-        params.put("location", "60%,1%");
-        driver.executeScript("mobile:touch:tap", params);
-
-        //
-        // swipe to expose safari
-        //
-        
-        params.clear();
-        params.put("start", "50%,90%");
-        params.put("end", "50%,10%");
-        driver.executeScript("mobile:touch:swipe", params);
-
-        //
-        // click Safari
-        //
-
-        params.clear();
-        params.put("value", "//cell[@name='Safari']");
-        params.put("framework", "perfectoMobile");
-        driver.executeScript("mobile:application.element:click", params);
-        
-        //
-        // swipe to bottom
-        //
-        
-        params.clear();
-        params.put("start", "50%,90%");
-        params.put("end", "50%,10%");
-        for (int i=0;i<3;i++){
-            driver.executeScript("mobile:touch:swipe", params);
-        }
-
-        //
-        // clear Cache
-        //
-        
-        params.clear();
-        params.put("value", "//*[starts-with(text(),'Clear History')]");
-        params.put("framework", "perfectoMobile");
-        driver.executeScript("mobile:application.element:click", params);
-        params.put("value", "//*[(@class='UIAButton' or @class='UIATableCell') and starts-with(@label,'Clear') and @isvisible='true']");
-        driver.executeScript("mobile:application.element:click", params);
-        
-        //
-        // below version 8 need to clear also data
-        //
-        
-        if ( osVer[0] < 8 )
+        try
         {
-            params.put("value", "//*[starts-with(text(),'Clear Cookies')]");
+            switchToContext(driver, "NATIVE_APP");
+
+            //
+            // get os version
+            //
+        
+            params.put("property", "osVersion");
+            String osVerStr = (String) driver.executeScript("mobile:handset:info", params);
+            int[] osVer = parseVersion( osVerStr );
+        
+
+            //
+            // Launch Settings Application on it's main page
+            //
+        
+            params.clear();
+            params.put("name", "Settings");
+            try {
+                driver.executeScript("mobile:application:close", params);
+            } catch (Exception e) {}
+            driver.executeScript("mobile:application:open", params);
+            sleep(1000);
+
+            //
+            // Scroll to Top
+            //
+        
+            params.clear();
+            params.put("location", "60%,1%");
+            driver.executeScript("mobile:touch:tap", params);
+
+            //
+            // swipe to expose safari
+            //
+
+            boolean found = false;
+            int count = 0;
+
+            while(( !found ) && ( count < 3 ))
+            {
+                params.clear();
+                params.put("content", "Safari");
+                String result = (String) driver.executeScript("mobile:checkpoint:text", params);
+
+                if ( !( found = (( result != null ) && (result.equalsIgnoreCase("true"))) ))
+                {
+                    params.clear();
+                    params.put("start", "50%,90%");
+                    params.put("end", "50%,10%");
+                    driver.executeScript("mobile:touch:swipe", params);
+
+                    ++count;
+                }
+            }
+
+            //
+            // click Safari
+            //
+
+            params.clear();
+            params.put("value", "//cell[@name='Safari']");
+            params.put("framework", "perfectoMobile");
+            driver.executeScript("mobile:application.element:click", params);
+        
+            //
+            // swipe to bottom
+            //
+        
+            params.clear();
+            params.put("start", "50%,90%");
+            params.put("end", "50%,10%");
+            for (int i=0;i<3;i++){
+                driver.executeScript("mobile:touch:swipe", params);
+            }
+
+            //
+            // clear Cache
+            //
+        
+            params.clear();
+            params.put("value", "//*[starts-with(text(),'Clear History')]");
+            params.put("framework", "perfectoMobile");
             driver.executeScript("mobile:application.element:click", params);
             params.put("value", "//*[(@class='UIAButton' or @class='UIATableCell') and starts-with(@label,'Clear') and @isvisible='true']");
             driver.executeScript("mobile:application.element:click", params);
-        }
+        
+            //
+            // below version 8 need to clear also data
+            //
+        
+            if ( osVer[0] < 8 )
+            {
+                params.put("value", "//*[starts-with(text(),'Clear Cookies')]");
+                driver.executeScript("mobile:application.element:click", params);
+                params.put("value", "//*[(@class='UIAButton' or @class='UIATableCell') and starts-with(@label,'Clear') and @isvisible='true']");
+                driver.executeScript("mobile:application.element:click", params);
+            }
 
-        //
-        // Close Settings
-        //
+            //
+            // Close Settings
+            //
         
-        params.clear();
-        params.put("name", "Settings");
+            params.clear();
+            params.put("name", "Settings");
         
-        driver.executeScript("mobile:application:close", params);
+            driver.executeScript("mobile:application:close", params);
+        }
+        finally
+        {
+            switchToContext(driver, currentContext);
+        }
     }
 
     public static void clearChromeAndroidCache( RemoteWebDriver driver )
@@ -114,91 +138,100 @@ public class BrowserCacheLogic
         HashMap<String, Object> params = new HashMap();
      
 
-        switchToContext(driver, "NATIVE_APP");
+        String currentContext = getCurrentContextHandle( driver );
 
-        //
-        // get os version
-        //
-        
-        params.put("property", "osVersion");
-        String osVerStr = (String) driver.executeScript("mobile:handset:info", params);
-        int[] osVer = parseVersion( osVerStr );
-
-        if ( osVer[0] >= 5 )
+        try
         {
+            switchToContext(driver, "NATIVE_APP");
+
             //
-            // Launch Chrome
+            // get os version
             //
         
-            params.clear();
-            params.put("name", "Chrome");
-            try {
+            params.put("property", "osVersion");
+            String osVerStr = (String) driver.executeScript("mobile:handset:info", params);
+            int[] osVer = parseVersion( osVerStr );
+
+            if ( osVer[0] >= 5 )
+            {
+                //
+                // Launch Chrome
+                //
+        
+                params.clear();
+                params.put("name", "Chrome");
+                try {
+                    driver.executeScript("mobile:application:close", params);
+                } catch (Exception e) {}
+                driver.executeScript("mobile:application:open", params);
+                sleep(1000);
+
+                //
+                // select chrome menu
+                //
+                // resource-id: com.android.chrome:id/document_menu_button -- contains menu_button
+                // content-desc: More options
+                //
+
+                params.clear();
+                params.put("value", "//*[starts-with(@resource-id,'menu_button')]");
+                params.put("framework", "perfectoMobile");
+                driver.executeScript("mobile:application.element:click", params);
+
+                //
+                // select history
+                //
+                // text: History
+                // resource-id: com.android.chrome:id/menu_item_text
+                // content-desc: History
+                //
+
+                params.clear();
+                params.put("value", "//*[starts-with(text(),'History')]");
+                params.put("framework", "perfectoMobile");
+                driver.executeScript("mobile:application.element:click", params);
+
+                //
+                // select clear browsing data
+                //
+                // text: Clear browsing data...
+                // id: clear-browsing-data
+                // cssSelector: #clear-browsing-data
+                //
+
+                params.clear();
+                params.put("value", "//*[starts-with(text(),'Clear browsing')]");
+                params.put("framework", "perfectoMobile");
+                driver.executeScript("mobile:application.element:click", params);
+
+                //
+                // do it!
+                //
+                // text: Clear
+                //
+
+                params.clear();
+                params.put("value", "//*[starts-with(text(),'Clear')]");
+                params.put("framework", "perfectoMobile");
+                driver.executeScript("mobile:application.element:click", params);
+
+                //
+                // Close Chrome
+                //
+        
+                params.clear();
+                params.put("name", "Chrome");
+        
                 driver.executeScript("mobile:application:close", params);
-            } catch (Exception e) {}
-            driver.executeScript("mobile:application:open", params);
-            sleep(1000);
-
-            //
-            // select chrome menu
-            //
-            // resource-id: com.android.chrome:id/document_menu_button -- contains menu_button
-            // content-desc: More options
-            //
-
-            params.clear();
-            params.put("value", "//*[starts-with(@resource-id,'menu_button')]");
-            params.put("framework", "perfectoMobile");
-            driver.executeScript("mobile:application.element:click", params);
-
-            //
-            // select history
-            //
-            // text: History
-            // resource-id: com.android.chrome:id/menu_item_text
-            // content-desc: History
-            //
-
-            params.clear();
-            params.put("value", "//*[starts-with(text(),'History')]");
-            params.put("framework", "perfectoMobile");
-            driver.executeScript("mobile:application.element:click", params);
-
-            //
-            // select clear browsing data
-            //
-            // text: Clear browsing data...
-            // id: clear-browsing-data
-            // cssSelector: #clear-browsing-data
-            //
-
-            params.clear();
-            params.put("value", "//*[starts-with(text(),'Clear browsing')]");
-            params.put("framework", "perfectoMobile");
-            driver.executeScript("mobile:application.element:click", params);
-
-            //
-            // do it!
-            //
-            // text: Clear
-            //
-
-            params.clear();
-            params.put("value", "//*[starts-with(text(),'Clear')]");
-            params.put("framework", "perfectoMobile");
-            driver.executeScript("mobile:application.element:click", params);
-
-            //
-            // Close Chrome
-            //
-        
-            params.clear();
-            params.put("name", "Chrome");
-        
-            driver.executeScript("mobile:application:close", params);
+            }
+            else
+            {
+                throw new IllegalStateException( "not supported before Android 5.0" );
+            }
         }
-        else
+        finally
         {
-            throw new IllegalStateException( "not supported before Android 5.0" );
+            switchToContext(driver, currentContext);
         }
     }
 
@@ -214,6 +247,15 @@ public class BrowserCacheLogic
         params.put("name", context);
         
         executeMethod.execute(DriverCommand.SWITCH_TO_CONTEXT, params);
+    }
+
+    private static String getCurrentContextHandle(RemoteWebDriver driver)
+    {          
+        RemoteExecuteMethod executeMethod = new RemoteExecuteMethod(driver);
+        
+        String context =  (String) executeMethod.execute(DriverCommand.GET_CURRENT_CONTEXT_HANDLE, null);
+        
+        return context;
     }
 
     private static void sleep(long millis) 
