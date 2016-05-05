3a2514344046414f383b4266c199fe1a64e40aeb
Allen Geary <ageary@morelandlabs.com>
2016-05-05 07:21:26 -0400

    Added new googleSearch project as an example

---
+++ googleSearch/src/com/morelandLabs/page/googleSearch/spi/SearchImpl.java
@@ -0,0 +1,15 @@
+package com.morelandLabs.page.googleSearch.spi;
+
+import com.morelandLabs.page.googleSearch.Search;
+import com.perfectoMobile.page.LocalAbstractPage;
+
+public class SearchImpl extends LocalAbstractPage implements Search
+{
+
+    public void initializePage()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
